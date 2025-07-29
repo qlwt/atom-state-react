ffef1c7a48ea2a6ec0044aeb915d8e7858778046
qte <>
2025-07-29 16:18:16 +0300

    realease 2.0.0
    update deps
    
    fix bug with atomfamily_child

--- src/hook/atom/child.ts
+++ src/hook/atom/child.ts
@@ -2,20 +2,20 @@ import { useAtomValue } from "#src/hook/atom/value.js";
 import type * as asc from "@qyu/atom-state-core";
 import * as react from "react";
 
-type UseAtomChild_Params<P extends readonly unknown[], V> = {
-    readonly params: P
-    readonly atomfamily: asc.AtomFamily<P, V>
+type UseAtomChild_Params<Index, V> = {
+    readonly params: Index
+    readonly atomfamily: asc.AtomFamily<Index, V>
 }
 
-export const useAtomChild = function <P extends readonly unknown[], V>(params: UseAtomChild_Params<P, V>): V {
+export const useAtomChild = function <Index, V>(params: UseAtomChild_Params<Index, V>): V {
     const { atomfamily, params: atomfamily_params } = params
 
     const family = useAtomValue(atomfamily)
 
     return react.useMemo(
         () => {
-            return family.reg(...atomfamily_params)
+            return family.reg(atomfamily_params)
         },
-        [family, ...atomfamily_params]
+        [family, atomfamily_params]
     )
 }
